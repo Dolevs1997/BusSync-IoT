088eeac09b4c9f8fe8d656e80e6b62ac8dee3080
Dolev <dolevs1890@gmail.com>
2024-09-22 23:30:30 +0300

    adding pins header

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,19 @@
 
 // #define BLYNK_PRINT Serial
 #include "config.h"
-#include <LiquidCrystal_I2C.h>
 #include "pitches.h"
+#include "pins.h"
+#include <LiquidCrystal_I2C.h>
 #include <BlynkSimpleEsp32.h>
 #include <WiFi.h>
 #include <Wire.h>
+
 // Initialize LCDs
 LiquidCrystal_I2C LCD1(0x27, 16, 4);
 LiquidCrystal_I2C LCD2(0x28, 16, 4);
 LiquidCrystal_I2C LCD3(0x29, 16, 4);
 LiquidCrystal_I2C LCD4(0x30, 16, 4);
 
-// Define virtual pins
-#define VIRTUAL_PIN_LED_1 V1
-#define VIRTUAL_PIN_LED_2 V2
-#define VIRTUAL_PIN_LED_3 V3
-#define VIRTUAL_PIN_LED_4 V4
-
-// I2C pins for LCDs
-#define I2C_SDA 21
-#define I2C_SCL 22
-
-// Speaker pin
-#define SPEAKER_PIN 25
-
-// Ultrasonic sensor pins
-#define echoPin1 15
-#define trigPin1 2
-#define echoPin2 18
-#define trigPin2 19
-#define echoPin3 13
-#define trigPin3 23
-#define echoPin4 32
-#define trigPin4 33
-
-// LED pins
-#define LED_PIN_1 5  // yellow
-#define LED_PIN_2 17 // green
-#define LED_PIN_3 16 // red
-#define LED_PIN_4 4  // blue
-
-// Button pins (Bus)
-#define BUTTON_BUS_1 5  // yellow
-#define BUTTON_BUS_2 17 // green
-#define BUTTON_BUS_3 16 // red
-#define BUTTON_BUS_4 4  // blue
-
-// Button pins (Station)
-#define BUTTON_PIN_1 26 // yellow
-#define BUTTON_PIN_2 27 // green
-#define BUTTON_PIN_3 14 // red
-#define BUTTON_PIN_4 12 // blue
 
 // WiFi credentials
 char ssid[] = "Wokwi-GUEST";
